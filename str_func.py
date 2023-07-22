108fefc2aa5e2c449a8c20c1b06655ff573479f9
AmeliPulen <139628158+AmeliPulen@users.noreply.github.com>
2023-07-22 22:56:11 +0300

    add upper in func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def _upper(value: str):
     return value.upper()
 
+
+def _title(value: str):
+    """
+    Функция возвращает слово с заглавной буквы
+    """
+    return value.title()
+
+print(_title('piska'))
